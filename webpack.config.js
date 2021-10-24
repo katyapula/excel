f04f7c6e1d5d0180b22197d9c9e5e380d90852ec
Romanuch4 <55475172+Romanuch4@users.noreply.github.com>
2021-10-24 22:58:36 +0300

    add rules for basic config (#2)

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,53 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.ts')
+  entry: {
+    main: path.resolve(__dirname, './src/index.ts'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[hash].js',
+  },
+  resolve: {
+    extensions: ['js'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@core': path.resolve(__dirname, 'src/core'),
     },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.bundle.js'
-    },
-    mode: "development",
-    devtool: "inline-source-map",
-    plugins: [new HtmlWebpackPlugin({
-        title: 'htmlZip',
-        template: path.resolve(__dirname, './src/template.html'),
-        filename: 'index.html'
-    })],
-    module: {
-        rules: [
-            // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-            { test: /\.tsx?$/, loader: "ts-loader", exclude: /node_modules/}
-        ]
-    }
+  },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'htmlZip',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/favicon.ico'),
+          to: path.resolve(__dirname, 'dist'),
+        },
+      ],
+    }),
+    new MiniCssExtractPlugin({ filename: 'index.[hash].css' }),
+  ],
+  module: {
+    rules: [
+      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
+      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+      //css
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+    ],
+  },
 };

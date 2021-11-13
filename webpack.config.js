9c82bc51234a3f154cc67574ac08913490657973
katyapula <katyapula@gmail.com>
2021-11-13 18:27:54 +0200

    babel, css-loader, ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,61 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/index.ts'),
+    main: path.resolve(__dirname, "./src/index.ts"),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.[hash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index.[hash].js",
   },
   resolve: {
-    extensions: ['js'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
     alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core'),
+      "@": path.resolve(__dirname, "src"),
+      "@core": path.resolve(__dirname, "src/core"),
     },
   },
-  mode: 'development',
-  devtool: 'inline-source-map',
+  mode: "development",
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'htmlZip',
-      template: path.resolve(__dirname, './src/template.html'),
-      filename: 'index.html',
+      title: "htmlZip",
+      template: path.resolve(__dirname, "./src/template.html"),
+      filename: "index.html",
     }),
     new CleanWebpackPlugin(),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist'),
+          from: path.resolve(__dirname, "src/favicon.ico"),
+          to: path.resolve(__dirname, "dist"),
         },
       ],
     }),
-    new MiniCssExtractPlugin({ filename: 'index.[hash].css' }),
+    new MiniCssExtractPlugin({ filename: "index.[hash].css" }),
   ],
   module: {
     rules: [
-      // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/ },
+      // all files with a `.ts` or `.tsx` extension will be handled by `babel-loader` (includes `ts-loader`)
+      {
+        test: /\.(js|jsx|tsx|ts)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          },
+        },
+      },
       //css
       {
         test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
     ],
   },

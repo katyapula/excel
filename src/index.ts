b597673a2716e49b1a32cfa56ce26cfdf87f5012
katyapula <katyapula@gmail.com>
2021-11-13 18:32:32 +0200

    babel, css-loader, ts (#3)

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-import './module.ts'
+// import './module.ts'
+// import "./file.css";
 
 console.log('Hello world (index)');
+async function test () {
+    await console.log('async');
+}
+
+test ();
+
+const A: string = 'Hello'
+alert(A);
